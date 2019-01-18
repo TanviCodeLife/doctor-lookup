f8c1aba8139fd76b61d7b57c1992db54dce45fbb
Tanvi Garg <tanvi.garg23@gmail.com>
2019-01-18 14:54:27 -0800

    add ui for search by name

--- src/main.js
+++ src/main.js
@@ -18,13 +18,56 @@ static resultsByDoctorName(name) {
   let namePromise = Doctor.getByName(name);
   namePromise.then((response) => {
     const doctorList = Doctor.getDoctorList(response);
-    doctorList.forEach(function(doctor){
-      $("#doctor-list").append(`<li>${doctor.firstName} ${doctor.lastName}</li>`);
-    });
-    }, (error) => {
-      console.log(error)
+    showResultsUi();
+    displayResults(doctorList);
+  });
+}, (error) => {
+  showResultsUi();
+  displayError(error.message);
+});
+}
+
+function displayResults(doctors) {
+  if (doctors.length === 0){
+    const doctorCard = `<div class='doctor-card'>
+    <p><i class="fas fa-exclamation-triangle"></i> No results match your search criteria. Please try again.</p>
+    </div>`;
+    $('#results').append(doctorCard);
+  } else {
+    doctors.forEach((doctor, index) => {
+      const doctorCard = `<div class='doctor-card'>
+      <h2>${doctor.firstName} ${doctor.lastName}</h2>
+      <ul id='${index}'>
+      </ul>
+      </div>`;
+      $('#results').append(doctorCard);
+
+      doctor.details.forEach((detail) => {
+        let phoneNumber = formatNumber(detail.phone);
+        let listItem = `<li class='detail-list-item'>
+        <h5>${detail.name}</h5>
+        ${detail.newPatients}<br/>
+        <a href='tel:${phoneNumber}'>${phoneNumber}</a><br/>
+        <a href='${detail.website}' target='_blank'>${detail.website}</a><br/>
+        ${detail.address.street}<br/>
+        ${detail.address.city}, ${detail.address.state} ${detail.address.zip}<br/>
+        </li>`;
+        $(`#${index}`).append(listItem);
+      });
     });
   }
+}
+
+function showResultsUi() {
+  $('#results').show();
+}
+
+function displayError(message) {
+  const doctorCard = `<div class='doctor-card'>
+  <p><i class="fas fa-exclamation-triangle"></i> There was an error processing your request: ${message}.</p>
+  </div>`;
+  $('.results-box').append(doctorCard);
+}
 
 $(document).ready(function() {
   $('#condition-form').submit(function(event) {
