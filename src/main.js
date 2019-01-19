5c8dfcbcd8805c33548783109303cc737f6f5311
Tanvi Garg <tanvi.garg23@gmail.com>
2019-01-18 16:19:01 -0800

    refactor results display function

--- src/main.js
+++ src/main.js
@@ -38,39 +38,48 @@ function parseNumber(phone) {
   return newPhone;
 }
 
-
-function displayResults(doctorsList) {
-  if (doctorsList.length === 0){
-    const doctorCard = `<div class='doctor-card'>
+function displayNoResults(doctorsList){
+  if (doctorsList.length === 0)
+   return const doctorCard = `<div class='doctor-card'>
     <p><i class="fas fa-exclamation-triangle"></i> No results match your search criteria. Please try again.</p>
     </div>`;
     $('#results').append(doctorCard);
-  } else {
-    doctorsList.forEach((doctor, index) => {
-      const doctorCard = `<div class='doctor-card'>
-      <h2>${doctor.firstName} ${doctor.lastName}</h2>
-      <p>${doctor.bio}</p>
-      <ul id='${index}'>
-      </ul>
-      </div>`;
-      $('#results').append(doctorCard);
+}
 
-      doctor.details.forEach((detail) => {
-        let phoneNumber = parseNumber(detail.phone);
-        let listItem = `<li class='detail-list-item'>
-        <h5>${detail.name}</h5>
-        ${detail.newPatients}<br/>
-        <a href='tel:${phoneNumber}'>${phoneNumber}</a><br/>
-        <a href='${detail.website}' target='_blank'>${detail.website}</a><br/>
-        ${detail.address.street}<br/>
-        ${detail.address.city}, ${detail.address.state} ${detail.address.zip}<br/>
-        </li>`;
-        $(`#${index}`).append(listItem);
-      });
+function displayDoctor(doctor, index){
+  const doctorCard = `<div class='doctor-card'>
+  <h2>${doctor.firstName} ${doctor.lastName}</h2>
+  <p>${doctor.bio}</p>
+  <ul id='${index}'>
+  </ul>
+  </div>`;
+  $('#results').append(doctorCard);
+
+}
+
+function displayPracticeDetails(doctor, index){
+  doctor.details.forEach((detail) => {
+    let phoneNumber = parseNumber(detail.phone);
+    let listItem = `<li class='detail-list-item'>
+    <h5>${detail.name}</h5>
+    ${detail.newPatients}<br/>
+    <a href='tel:${phoneNumber}'>${phoneNumber}</a><br/>
+    ${detail.address.street}<br/>
+    ${detail.address.city}, ${detail.address.state} ${detail.address.zip}<br/>
+    </li>`;
+    $(`#${index}`).append(listItem);
+  });
+}
+
+
+function checkAndDisplayResults(doctorsList) {
+  displayNoResults(doctorsList);
+  doctorsList.forEach((doctor, index) => {
+    displayDoctor(doctor, index);
+    displayDetails(doctor, index);
     });
   }
 }
-
 
 function showResultsUi() {
   $('#results').show();
